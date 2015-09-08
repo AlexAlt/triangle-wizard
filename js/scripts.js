af2a5b924d276683763ed360f88dae1136ab56c1
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-08 11:33:52 -0700

    Display on web page what type of triangle it is

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,26 @@ var triangle = function(side1, side2, side3) {
     return "scalene";
   }
 };
+
+$(document).ready(function(){
+  $("form#triangle").submit(function(event){
+    $(".not").hide();
+    $(".triangle").hide();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = triangle(side1, side2, side3);
+
+    if (!result) {
+      $(".not").text("not")
+      $(".not").show();
+    } else {
+      $(".triangle").text(result)
+      $(".triangle").show();
+    }
+
+    $("#result").show();
+    event.preventDefault();
+
+  })
+})
