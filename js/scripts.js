0872cc3ed61f2c4cd6640939e4087ca76b3ab878
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-08 11:06:17 -0700

    Return true if it is an equilateral or isosceles triangle, and false if it is not a triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var triangle = function(side1, side2, side3) {
-  return true;
+  if ((side1 + side2 < side3) || (side1 + side3 < side2) || (side3 + side2 < side1)) {
+    return false;
+  } else if ((side1 === side2) && (side3 === side2) && (side1 === side3)) {
+    return true;
+  } else if ((side1 === side2) || (side3 === side2) || (side1 === side3)) {
+    return true;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
 describe('triangle', function() {
-  it("returns true if it is a triangle", function() {
+  it("returns false if it is not a triangle", function() {
+    expect(triangle(2, 2, 5)).to.equal(false);
+  });
+
+  it("returns true if it is an equilateral triangle", function() {
     expect(triangle(2, 2, 2)).to.equal(true);
   });
+
+  it("returns true if it is an isosceles triangle", function () {
+    expect(triangle(2, 2, 1)).to.equal(true);
+  });
+
 });
