b950eedce2fe479c4970aaa13cda1e353d15648b
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-08 11:10:16 -0700

    Return true if it is a scalene triangle

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,7 @@ var triangle = function(side1, side2, side3) {
     return true;
   } else if ((side1 === side2) || (side3 === side2) || (side1 === side3)) {
     return true;
+  } else if ((side1 !== side2) && (side3 !== side2) && (side1 !== side3)) {
+    return true;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -7,8 +7,12 @@ describe('triangle', function() {
     expect(triangle(2, 2, 2)).to.equal(true);
   });
 
-  it("returns true if it is an isosceles triangle", function () {
+  it("returns true if it is an isosceles triangle", function() {
     expect(triangle(2, 2, 1)).to.equal(true);
+  });
+
+  it("returns true if it is a scalene", function() {
+    expect(triangle(3, 4, 5)).to.equal(true);
   });
 
 });
